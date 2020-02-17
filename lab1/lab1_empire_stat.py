b6c9fda310d64a9a11a3c31a42e973b8af95886b
juliiette <stnk.jl@gmail.com>
2020-02-17 15:35:49 +0200

    from 1 to 4

--- lab1/lab1_empire_stat.py
+++ lab1/lab1_empire_stat.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import lab1.my_array as mod
 from statistics import median
+import statsmodels.api as sm
 
 array = mod.ret_array()
 sorted_array = sorted(array)
 
 def interval_variation_row(array):
         l = math.floor(1 + 3.32*math.log(len(array), 10)) # intervals quantity
-        h = (np.amax(array) - np.amin(array)) / l # длина интервала групировки 12.875
+        h = (np.amax(array) - np.amin(array)) / l # длина интервала групировки
         array_min = np.amin(array)
 
         interval_row = {}
@@ -28,7 +29,19 @@ def interval_variation_row(array):
                         interval.append(i)
 
                 interval_row[key] = sorted(interval)
-        return interval_row, h
+        return interval_row
+
+
+def funded_relative_freq_arr(row): # 4 zadanie dlya histogrami
+        funded_relative = len(row[1]) / 100
+        funded_relative_arr = [funded_relative]
+        for key in row:
+                frequency = len(row[key])
+                relative_frequency = frequency / 100
+                if row[key] != row[1]:
+                        funded_relative += relative_frequency
+                        funded_relative_arr.append(funded_relative)
+        return funded_relative_arr
 
 
 def group_sampling_frequency_table(row):
@@ -49,20 +62,31 @@ def group_sampling_frequency_table(row):
                       % (key, low, high, mediana, frequency, relative_frequency, funded_frequency, funded_relative))
 
 
-def histogram_polygon(row):
+def medians_frequency_histogram(row):
         x = []
         y = []
         for key in row:
-                x.append(np.amin(row[key]))
+                x.append(median(row[key]))
                 y.append(len(row[key]) / 100)
-        width = 12.875
-        plt.bar(x, y, width, edgecolor='black')
+        width = 12
+        plt.bar(x, y, width, edgecolor='black', color='white')
         plt.plot(x, y, 'r--')
-
         plt.xticks(x)
         plt.show()
 
 
+def medians_funded_relative_histogram(row):
+    x = []
+    y = funded_relative_freq_arr(row)
+    for key in row:
+        x.append(median(row[key]))
+    width = 12
+    plt.bar(x, y, width, edgecolor='black', color='white')
+    plt.plot(x, y, 'r--')
+    plt.xticks(x)
+    plt.show()
+
+
 interval_row = interval_variation_row(sorted_array)
-histogram_polygon(interval_row)
-#  group_sampling_frequency_table(interval_row)
+#medians_funded_relative_histogram(interval_row)
+#group_sampling_frequency_table(interval_row)
